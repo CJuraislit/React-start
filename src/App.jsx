c7dc1beb5299fbbdb2aa6269e07f88bc0486a941
CJura <korolevpavel02@gmail.com>
2024-03-22 14:39:18 +0300

    added IntroSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import Header from "./components/Header";
 import TeachingSection from "./components/TeachingSection.jsx";
 import DifferencesSection from "./components/DifferencesSection.jsx";
+import IntroSection from "./components/IntroSection.jsx";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <main>
+        <IntroSection />
         <TeachingSection />
         <DifferencesSection />
       </main>
-    </div>
+    </>
   );
 }
 
