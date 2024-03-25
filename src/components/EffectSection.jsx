9b4dcdf193e73e0c2b645b880211622b07c90014
CJ laptop <korolevpavel02@gmial.com>
2024-03-25 19:55:16 +0300

    added fetch with useEffect

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,19 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./Button/Button";
 import Modal from "./Modal/Modal";
 
 export default function EffectSecton() {
   const [modal, setModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
 
-  function openModal() {
-    setModal(true);
+  async function fecthUsers() {
+    setLoading(true);
+    const response = await fetch(" https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    setUsers(users);
+    setLoading(false);
   }
+
+  useEffect(() => {
+    fecthUsers();
+  }, []);
 
   return (
     <section>
       <h3>Effects</h3>
 
-      <Button onClick={openModal}>Открыть информацию</Button>
+      <Button style={{ marginBottom: "1rem" }} onClick={() => setModal(true)}>
+        Открыть информацию
+      </Button>
 
       <Modal open={modal}>
         <h3>Hello from Modal</h3>
@@ -26,6 +38,16 @@ export default function EffectSecton() {
 
         <Button onClick={() => setModal(false)}>Close Modal</Button>
       </Modal>
+
+      {loading && <p>Loading...</p>}
+
+      {!loading && (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import logo from "/vite.svg";
 import { styled } from "styled-components";
 // import "./Header.css";
@@ -16,7 +16,13 @@ const HeaderContainer = styled.header`
 export default function Header() {
   const [now, setNow] = useState(new Date());
 
-  setInterval(() => setNow(new Date()), 1000);
+  useEffect(() => {
+    const interval = setInterval(() => setNow(new Date()), 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <HeaderContainer>
